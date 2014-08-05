16afcea4b5a966222dbeb5ad6116cde66d6c5762
Abil N George <mail@abilng.in>
2014-08-05 13:22:46 +0530

    Added: checkConfig fn

--- run_RBM.py
+++ run_RBM.py
@@ -1,3 +1,7 @@
+#!/usr/bin/env python2.7
+# Copyright 2014	G.K SUDHARSHAN <sudharpun90@gmail.com>    IIT Madras
+# Copyright 2014	Abil N George<mail@abilng.in>    IIT Madras
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
@@ -12,6 +16,16 @@
 # limitations under the License.
 
 
-if __name__ == '__main__':
+def runRBM(configFile):
+	model_config = load_model(configFile)
 
+	if checkConfig(model_config,'RBM'):
+		print "Error: the mandatory arguments are missing in model properties file.."
+		exit(1)
+	pass
+
+
+
+if __name__ == '__main__':
     import sys
+    runRBM(sys.argv[1])
