73daacec174ee7669d110381fe43392c3dd92333
Abil N George <mail@abilng.in>
2014-08-21 14:02:08 +0530

    Fix:MINOR:CNN

--- run_CNN.py
+++ run_CNN.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python2.7
-# Copyright 2014	G.K SUDHARSHAN <sudharpun90@gmail.com>    IIT Madras
-# Copyright 2014	Abil N George<mail@abilng.in>    IIT Madras
+# Copyright 2014	G.K SUDHARSHAN <sudharpun90@gmail.com>	IIT Madras
+# Copyright 2014	Abil N George<mail@abilng.in>	IIT Madras
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -30,71 +30,58 @@ from utils.utils import parse_activation
 from io_modules.model_io import _cnn2file,_nnet2file
 from io_modules import setLogger
 
+from models import fineTunning,testing
+
 import logging
 logger = logging.getLogger(__name__)
 
 
 def runCNN(configFile):
 	model_configs = load_model(configFile,'CNN')
-
-	# learning rate, batch-size and momentum
-	lrate = LearningRate.get_instance(model_configs['l_rate_method'],model_configs['l_rate']);
-	batch_size = model_configs['batch_size'];
-	momentum = model_configs['momentum']
-
-
 	conv_configs,conv_layer_configs = load_conv_spec(model_configs['conv_nnet_spec'],model_configs['batch_size'],
 				model_configs['input_shape'])
-	
+
 	mlp_configs = load_mlp_spec(model_configs['hidden_nnet_spec']);
-	
 	data_spec =  load_data_spec(model_configs['data_spec']);
 	
-	train_sets, train_xy, train_x, train_y = read_dataset(data_spec['training'])
-	valid_sets, valid_xy, valid_x, valid_y = read_dataset(data_spec['validation'])
-
 	numpy_rng = numpy.random.RandomState(89677)
 	theano_rng = RandomStreams(numpy_rng.randint(2 ** 30))
 	
 	logger.info('> ... building the model')
 	conv_activation = parse_activation(conv_configs['activation']);
 	hidden_activation = parse_activation(mlp_configs['activation']);
-	
+
+	#learning rate, batch-size and momentum
+	lrate = LearningRate.get_instance(model_configs['l_rate_method'],model_configs['l_rate']);
+	batch_size = model_configs['batch_size'];
+	momentum = model_configs['momentum']
+
 	cnn = CNN(numpy_rng,theano_rng,conv_layer_configs = conv_layer_configs, batch_size = batch_size,
 		n_outs=model_configs['n_outs'],hidden_layers_sizes=mlp_configs['layers'], conv_activation = conv_activation,
 		hidden_activation = hidden_activation,use_fast = conv_configs['use_fast'])
 
-	logger.info('> ... getting the finetuning functions')
-	train_fn, valid_fn = cnn.build_finetune_functions((train_x, train_y),
-			 (valid_x, valid_y), batch_size=model_configs['batch_size'])
+	train_sets, train_xy, train_x, train_y = read_dataset(data_spec['training'])
+	valid_sets, valid_xy, valid_x, valid_y = read_dataset(data_spec['validation'])
 
-	start_time = time.clock()
-	while (lrate.get_rate() != 0):
-		train_error = []
-		while not train_sets.is_finish():
-			train_sets.make_partition_shared(train_xy)
-			for batch_index in xrange(train_sets.cur_frame_num / batch_size):  # loop over mini-batches
-				train_error.append(train_fn(index=batch_index, learning_rate = lrate.get_rate(), momentum = momentum))
-				logger.debug('>>>> training batch %d error %f' % (batch_index, numpy.mean(train_error)))
-			train_sets.read_next_partition_data()
-		logger.info('> epoch %d, training error %f' % (lrate.epoch, numpy.mean(train_error)))
-		train_sets.initialize_read()
-	
-	
-		valid_error = []
-		while (not valid_sets.is_finish()):
-			valid_sets.make_partition_shared(valid_xy)
-			for batch_index in xrange(valid_sets.cur_frame_num / batch_size):  # loop over mini-batches
-				valid_error.append(valid_fn(index=batch_index))
-				logger.debug('>>>> validation batch %d error %f' % (batch_index, numpy.mean(train_error)))
-			valid_sets.read_next_partition_data()
-		logger.info('> epoch %d, lrate %f, validation error %f' % (lrate.epoch, lrate.get_rate(), numpy.mean(valid_error)))
-		valid_sets.initialize_read()
-		lrate.get_next_rate(current_error = 100 * numpy.mean(valid_error))
-
+	err=fineTunning(cnn,train_sets,train_xy,train_x,train_y,
+		valid_sets,valid_xy,valid_x,valid_y,lrate,momentum,batch_size);
 	
 	_cnn2file(cnn.layers[0:cnn.conv_layer_num], filename=model_configs['conv_output_file'],activation=conv_configs['activation']);
 	_nnet2file(cnn.layers[cnn.conv_layer_num:], filename=model_configs['hidden_output_file'],activation=mlp_configs['activation']);
+
+	####################
+	##	TESTING	 ##
+	####################
+	try:
+		test_sets, test_xy, test_x, test_y = read_dataset(data_spec['testing']) 
+	except KeyError:
+		#raise e
+		logger.info("No testing set:Skiping Testing");
+		logger.info("Finshed")
+		sys.exit(0)
+
+	pred,err=testing(cnn,test_sets, test_xy, test_x, test_y,batch_size)
+
 	
 if __name__ == '__main__':
 	setLogger();
